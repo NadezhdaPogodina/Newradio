4f4d28b7d228034ed38482681c486113c4817e5d
NadezhdaPogodina <pogodinadia@inbox.ru>
2023-02-13 23:33:08 +0300

    Initial commit

---
+++ src/main/java/ru/netology/newradio/services/NewRadio.java
@@ -0,0 +1,101 @@
+package ru.netology.newradio.services;
+public class NewRadio {
+
+ public int currentRadioStation;
+
+    public int getCurrentRadioStation() {
+
+        return currentRadioStation;
+    }
+
+    public void Next(int newCurrentRadioStation) {
+
+        if (newCurrentRadioStation == 9) {
+
+            currentRadioStation = 0;
+        }
+
+        if (newCurrentRadioStation < 9) {
+
+            currentRadioStation = newCurrentRadioStation + 1;
+
+            if (newCurrentRadioStation >= 0) {
+
+                currentRadioStation = newCurrentRadioStation + 1;
+            }
+        }
+
+    }
+
+
+    public void Prev(int newCurrentRadioStation) {
+
+        if (newCurrentRadioStation == 0) {
+
+            currentRadioStation = 9;
+        }
+
+        if (newCurrentRadioStation > 0) {
+
+            currentRadioStation = newCurrentRadioStation - 1;
+
+            if (newCurrentRadioStation <= 9) {
+
+                currentRadioStation = newCurrentRadioStation - 1;
+            }
+        }
+    }
+
+
+    public int currentRadioVolume;
+
+    public int getCurrentRadioVolume() {
+
+        return currentRadioVolume;
+    }
+
+    public void maxRadioVolume(int newMaxRadioVolume) {
+
+        if (newMaxRadioVolume == 10) {
+
+            currentRadioVolume = newMaxRadioVolume + 1;
+            currentRadioVolume = 10;
+        }
+
+        if (newMaxRadioVolume < 10) {
+
+            currentRadioVolume = newMaxRadioVolume + 1;
+
+            if (newMaxRadioVolume > 0) {
+
+                currentRadioVolume = newMaxRadioVolume + 1;
+            }
+        }
+    }
+
+
+    public void minRadioVolume(int newMinRadioVolume) {
+
+        if (newMinRadioVolume == 0) {
+            currentRadioVolume = newMinRadioVolume - 1;
+            currentRadioVolume = 0;
+        }
+
+        if (newMinRadioVolume > 0) {
+
+            currentRadioVolume = newMinRadioVolume - 1;
+
+            if (newMinRadioVolume <= 10) {
+                currentRadioVolume = newMinRadioVolume - 1;
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
